52cd9de32da21f61175a224e1183c55681178a57
ArttuKnowit <arttu.taipale@knowit.fi>
2026-05-25 09:32:46 +0300

    Final fix

--- libraries/OperatorLib.py
+++
@@ -1,9 +0,0 @@
-from robot.api.deco import library, keyword
-from robot.api import logger
-
-@library
-class OperatorLib:
-    @keyword
-    def confirm_user_has_pressed_button(self):
-        logger.info("Waiting for user to press the button on the Arduino...", also_console=True)
-        input("Press Enter after pressing the button on the Arduino...")
